4eac4ac707d7a5fbb76e3e67144184a10dc6705f
CheezItMan <chris@adadevelopersacademy.org>
2020-04-04 18:41:28 -0700

    Board component

---
+++ src/components/Board.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './Board.css';
+import Square from './Square';
+import PropTypes from 'prop-types';
+
+
+const generateSquares = (squares, onClickCallback) => {
+  const singleArraySquares = [].concat(...squares);
+  console.log(squares);
+  console.log(singleArraySquares);
+  return singleArraySquares.map((square) => {
+    return <Square
+      value={square.value}
+      id={square.id}
+      onClickCallback={onClickCallback}
+      key={square.id}
+    />
+  });
+}
+
+const Board = ({ squares, onClickCallback }) => {
+
+  const squareList = generateSquares(squares, onClickCallback);
+  console.log(squareList);
+  return <div className="grid" >
+    {squareList}
+  </div>
+}
+
+Board.propTypes = {
+  squares: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    value: PropTypes.string.isRequired
+  }))),
+  onClickCallback: PropTypes.func.isRequired,
+};
+
+export default Board;
