2eed3e489cbc383c9d6d5760603d1ee86fe3a3ec
CheezItMan <chris@adadevelopersacademy.org>
2020-04-04 18:45:59 -0700

    add generate squares function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Board from './components/Board';
+
+const PLAYER_1 = 'x';
+const PLAYER_2 = 'o';
+
+const generateSquares = () => {
+  const squares = [];
+  let row = 0;
+  let col = 0;
+  let currentId = 0;
+  console.log(`row = ${ row }`);
+
+  while (row < 3) {
+    squares.push([]);
+    while (col < 3) {
+      squares[row].push({
+        id: currentId,
+        value: '',
+      });
+      col += 1;
+      currentId += 1;
+    }
+    row += 1;
+    col = 0;
+  }
+
+  return squares;
 }
+
+const App = () => {
+  return <div />
+};
+
 
 export default App;
