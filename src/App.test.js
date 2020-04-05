e4445066ae59b0ca6ca5cbe150e429f2557160f5
CheezItMan <chris@adadevelopersacademy.org>
2020-04-04 18:43:23 -0700

    fix App test file

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = getByText(/React Tic Tac Toe/i);
+  expect(titleElement).toBeInTheDocument();
 });
