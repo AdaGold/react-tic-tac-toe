1a182d6597d7abbbb5584561410703ea0675d2b1
CheezItMan <chris@adadevelopersacademy.org>
2020-04-04 19:06:05 -0700

    checking for a winner and updated CSS

--- src/App.js
+++ src/App.js
@@ -31,8 +31,58 @@ const generateSquares = () => {
 }
 
 const App = () => {
-  return <div />
-};
+
+  const [squares, setSquares] = useState(generateSquares());
+  const [currentPlayer, setCurrentPlayer] = useState(PLAYER_1);
+  const [numSquaresFilled, setNumSquaresFilled] = useState(0);
+  const [winner, setWinner] = useState(null);
+
+  const checkForWinner = () => {
+    let i = 0;
+
+    // Check all the rows and columns for a winner
+    while (i < 3) {
+      if (squares[i][0].value === squares[i][1].value &&
+        squares[i][2].value === squares[i][1].value &&
+        squares[i][0].value !== '') {
+        return squares[i][0].value;
+      } else if (squares[0][i].value === squares[1][i].value &&
+        squares[2][i].value === squares[1][i].value &&
+        squares[0][i].value !== '') {
+        return squares[0][i].value;
+      }
+      i += 1;
+    }
+    if (squares[0][0].value === squares[1][1].value &&
+      squares[2][2].value === squares[1][1].value &&
+      squares[1][1].value !== '') {
+      return squares[0][0].value;
+    }
+
+    if (squares[0][2].value === squares[1][1].value &&
+      squares[2][0].value === squares[1][1].value &&
+      squares[1][1].value !== '') {
+      return squares[0][2].value;
+    }
+
+    return null;
+  }
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>React Tic Tac Toe</h1>
+        <h2>{winner === null ? `Current Player ${ currentPlayer }` : `Winner is ${ winner }`}</h2>
+        <button onClick={resetGame}>Reset Game</button>
+      </header>
+      <main>
+        <Board squares={squares} onClickCallback={updateSquares} />
+      </main>
+    </div>
+  );
+}
+
 
 
 export default App;
