147772dd493f02bda1a5e0ae6ca5f10887b0a219
KARLA TORRES <t.karla212@gmail.com>
2021-06-25 12:05:04 -0400

    working on wave 1: represent the state of teh game board in 2D array object, reder a board, create 9 squares which display the value passed in it.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import square from './components/Board';
 
 ReactDOM.render(
   <React.StrictMode>
