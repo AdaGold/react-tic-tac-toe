51c68e025761bfeb020551af085820c255de8bd3
Chris McAnally <chris@adadevelopersacademy.org>
2021-11-12 17:29:18 -0800

    fix merge conflict

--- src/components/Board.test.js
+++ src/components/Board.test.js
@@ -1,100 +1,96 @@
-import React from 'react';
-import '@testing-library/jest-dom/extend-expect';
-import Board from './Board';
-import { render, screen, fireEvent} from '@testing-library/react'
+import React from "react";
+import "@testing-library/jest-dom/extend-expect";
+import Board from "./Board";
+import { render, screen, fireEvent } from "@testing-library/react";
 
-
-describe('Wave 1: Board', () => {
+describe("Wave 1: Board", () => {
   // Sample input to the Board component
   const SAMPLE_BOARD = [
     [
       {
-        value: 'X',
+        value: "X",
         id: 0,
       },
       {
-        value: 'X',
+        value: "X",
         id: 1,
       },
       {
-        value: 'O',
+        value: "O",
         id: 2,
       },
     ],
     [
       {
-        value: 'X',
+        value: "X",
         id: 3,
       },
       {
-        value: 'X',
+        value: "X",
         id: 4,
       },
       {
-        value: 'O',
+        value: "O",
         id: 5,
       },
     ],
     [
       {
-        value: 'O',
+        value: "O",
         id: 6,
       },
       {
-        value: 'O',
+        value: "O",
         id: 7,
       },
       {
-        value: 'X',
+        value: "X",
         id: 8,
       },
-    ],    
+    ],
   ];
 
-  test('that board will render with the proper number of Xs and Os', () => {
+  test("that board will render with the proper number of Xs and Os", () => {
     // Act
-    render(<Board squares={SAMPLE_BOARD} onClickCallback={() => { }} />);
-      
+    render(<Board squares={SAMPLE_BOARD} onClickCallback={() => {}} />);
+
     // Assert
-    const xSquares = screen.getAllByText('X');
+    const xSquares = screen.getAllByText("X");
     expect(xSquares.length).toEqual(5);
 
-    const oSquares = screen.getAllByText('O');
+    const oSquares = screen.getAllByText("O");
     expect(oSquares.length).toEqual(4);
   });
 
-  test('that the board can render empty squares', () => {
+  test("that the board can render empty squares", () => {
     // Arrange
     const emptyBoard = SAMPLE_BOARD.map((row) => {
       return row.map((square) => {
         return {
           ...square,
-          value: '',
-        }
+          value: "",
+        };
       });
     });
 
     // Act
-    const { container } = render(<Board squares={emptyBoard} onClickCallback={() => { }} />);
+    const { container } = render(
+      <Board squares={emptyBoard} onClickCallback={() => {}} />
+    );
 
     // Assert
-    const buttons = container.querySelectorAll('.grid button');
+    const buttons = container.querySelectorAll(".grid button");
     expect(buttons.length).toEqual(9);
   });
-<<<<<<< HEAD
 
-  describe('button click callbacks', () => {
-    test('that the callback is called for the 1st button', () => {
-=======
-});
-describe('Wave 2: Board', () => {
-  describe('button click callbacks', () => {
-    test.skip('that the callback is called for the 1st button', () => {
->>>>>>> main
+  describe("button click callbacks", () => {
+    test("that the callback is called for the 1st button", () => {
       // Arrange
       const callback = jest.fn();
-      const { container } = render(<Board squares={SAMPLE_BOARD} onClickCallback={callback} />);
-      const buttons = container.querySelectorAll('.grid button');
+      const { container } = render(
+        <Board squares={SAMPLE_BOARD} onClickCallback={callback} />
+      );
+      const buttons = container.querySelectorAll(".grid button");
 
       // Act
       fireEvent.click(buttons[0]);
@@ -103,15 +99,13 @@ describe('Wave 2: Board', () => {
       expect(callback).toHaveBeenCalled();
     });
 
-<<<<<<< HEAD
-    test('that the callback is called for the last button', () => {
-=======
-    test.skip('that the callback is called for the last button', () => {
->>>>>>> main
+    test("that the callback is called for the last button", () => {
       // Arrange
       const callback = jest.fn();
-      const { container } = render(<Board squares={SAMPLE_BOARD} onClickCallback={callback} />);
-      const buttons = container.querySelectorAll('.grid button');
+      const { container } = render(
+        <Board squares={SAMPLE_BOARD} onClickCallback={callback} />
+      );
+      const buttons = container.querySelectorAll(".grid button");
 
       // Act
       fireEvent.click(buttons[buttons.length - 1]);
