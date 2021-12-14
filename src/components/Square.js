5c65394580ba9dc20d9ede42cd6501f73b3bac4a
claire <yue.katou@gmail.com>
2021-12-13 16:34:22 -0800

    fixes semicolon warnings

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import './Square.css'
+import './Square.css';
 
 const Square = (props) => {
   // For Wave 1 enable this 
   //  Component to alert a parent 
   //  component when it's clicked on.
 
-  return <button
-    className="square"
-  >
-    {props.value}
-  </button>
-}
+  return <button className='square'>{props.value}</button>;
+};
 
 Square.propTypes = {
   value: PropTypes.string.isRequired,
